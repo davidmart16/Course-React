18841df444e9662bcf1dd5cb66575f580cd6216f
David Martinez <tinezillo_94@hotmail.com>
2021-11-23 15:44:09 +0100

    Working in progress

--- 01-Router-App/intro-routing/src/App.js
+++ 01-Router-App/intro-routing/src/App.js
@@ -1,9 +1,25 @@
+import {Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      hiiiiiii
-    </div>
+    <MainNavigation/>
+    <Routes>
+      <Route exact={true} path='/'>
+        <AllMeetupsPage/>
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage/>
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage/>
+      </Route>
+    </Routes>
+    </div> 
   );
 }
 
---
+++ 01-Router-App/intro-routing/src/components/layout/MainNavigation.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+function MainNavigation() {
+    return (
+    <header>
+        <div></div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>)
+}
+
+export default MainNavigation
